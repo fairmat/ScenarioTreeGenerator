d086be68e425e639e8668c1ec79b0cdd8aa6f816
mattia-trevisani-fairmat <135018591+mattia-trevisani-fairmat@users.noreply.github.com>
2024-05-16 14:43:57 +0200

    [RD-FFK-NET8] Net standard upgrade fairmat/dev/fairmatserver#3493 (#3)
    * [RD-FFK-NET8] Added vs folder to ignore + upgraded projects to sdk style with multitargeting fairmat/dev/fairmatserver#3493
    
    * [RD-FFK-NET8] Updated csproj for ui project fairmat/dev/fairmatserver#3493
    
    * [RD-FFK-NET8] Added to the gitignore the rider files. fairmat/dev/fairmatserver#3493
    
    * [RD-FFK-NET8] Applied some fixes while validating. fairmat/dev/fairmatserver#3493
    
    ---------
    
    Co-authored-by: Giulio Fornasaro <giulio.fornasaro@fairmat.com>

--- ScenarioTreeGenerator.UI/Properties/AssemblyInfo.cs
+++ ScenarioTreeGenerator.UI/Properties/AssemblyInfo.cs
@@ -16,47 +16,14 @@
  */
 
 using System.Reflection;
-using System.Runtime.InteropServices;
 using Mono.Addins;
 
 // The following lines tell that the assembly is an addin
-[assembly: Mono.Addins.Addin("Scenario Tree Generator", "1.0.3", Category = "Export")]
-[assembly: Mono.Addins.AddinDependency("Fairmat", "1.0")]
-[assembly: Mono.Addins.AddinAuthor("Fairmat SRL")]
+[assembly: Addin("Scenario Tree Generator", "1.0.3", Category = "Export")]
+[assembly: AddinDependency("Fairmat", "1.0")]
+[assembly: AddinAuthor("Fairmat SRL")]
 [assembly: AddinDescription("Scenario Tree Generator")]
-
 // This plug-in is also an addin root.
-[assembly: Mono.Addins.AddinRoot("ScenarionTreeGenerator", "1.0")]
+[assembly: AddinRoot("ScenarionTreeGenerator", "1.0")]
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Scenario Tree Generator")]
-[assembly: AssemblyDescription("Scenario Tree Generator")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Fairmat SRL")]
-[assembly: AssemblyCopyright("Copyright © Fairmat SRL 2009-2013")]
-[assembly: AssemblyProduct("Scenario Tree Generator")]
 [assembly: AssemblyTrademark("Fairmat")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("706dc359-e8fe-486d-8491-c4aefcb46940")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3")]
-[assembly: AssemblyFileVersion("1.0.3")]
